7562466d4c7ec690923f34b8df6b36eadfd5bd4f
zdm <zdm@softvisio.net>
2025-09-01 08:29:32 +0300

    fix: remove postgres from image

--- images/setup.sh
+++ images/setup.sh
@@ -3,8 +3,6 @@
 set -Eeuo pipefail
 trap 'echo -e "⚠  Error ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null | grep -oE "\S.*\S|\S" || true)" >&2; return 3 2> /dev/null || exit 3' ERR
 
-postgresql_version=17
-
 apt-get update
 
 apt-get install -y curl
@@ -13,5 +11,4 @@ script=$(curl -fsSL "https://raw.githubusercontent.com/softvisio/scripts/main/se
 source <(echo "$script")
 
 apt-get install -y \
-    apt-utils git gcc g++ make cmake libssl-dev gpg \
-    postgresql-server-dev-$postgresql_version libkrb5-dev libipc-run-perl
+    apt-utils git gcc g++ make cmake libssl-dev gpg
